85607257091a5b03b6e29df644fba66f5a243e37
bsalin39 <bsalin39@stu.southtexascollege.edu>
2022-11-23 20:38:08 -0600

    working on fish page

--- src/views/FishiesPage.vue
+++ src/views/FishiesPage.vue
@@ -1,13 +1,24 @@
 <script setup></script>
 
 <template>
-  <main>
-    <h1 class="content">I'm fish page</h1>
-  </main>
+  <h1 class="content">I'm fish page</h1>
+  <div class="fishcards">
+    <RouterLink v-for="n in 25" :key="n" :to="`/fish/${n}`" class="fish">Fish - {{ n }}</RouterLink>
+  </div>
 </template>
 
 <style lang="postcss" scoped>
 .content {
   @apply text-6xl font-thin text-slate-800;
+}
+
+.fishcards {
+  @apply grid flex-grow grid-cols-5 gap-x-4 gap-y-4;
+  & .fish {
+    @apply flex h-32 w-40 items-center justify-center rounded-lg border-4 border-green-500 py-4 font-bold uppercase text-slate-600 transition-colors duration-300;
+    &:hover {
+      @apply cursor-pointer border-red-500 bg-red-500 text-white;
+    }
+  }
 }
 </style>
